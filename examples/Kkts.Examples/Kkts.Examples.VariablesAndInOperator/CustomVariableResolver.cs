d2a5b20a8b88c09e39d3bde69dec589f5acd3e16
Linh Le <lenhatlinh@hotmail.com>
2023-10-16 00:52:27 +0700

    Implement Examples

---
+++ examples/Kkts.Examples/Kkts.Examples.VariablesAndInOperator/CustomVariableResolver.cs
@@ -0,0 +1,30 @@
+﻿using Kkts.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kkts.Examples.VarablesAndInOperator
+{
+    public class CustomVariableResolver : Expressions.VariableResolver
+    {
+        private readonly TestDbContext _context;
+        public CustomVariableResolver(TestDbContext context) 
+        {
+            _context = context;
+        }
+
+        public Project? Project { get; set; }
+
+        protected override async Task<VariableInfo> TryResolveCore(string name, CancellationToken cancellationToken)
+        => name.ToLower() switch
+        {
+            "userids" => new VariableInfo { Name = name, Resolved = true, Value = await _context.Users.Select(p => p.Id).Take(10).ToListAsync() },
+            _ => await base.TryResolveCore(name, cancellationToken),
+        };
+
+        public override Task InitializeVariablesAsync(object state)
+        {
+            TryAdd("User.Id", _context.Users.First().Id);
+            Project = _context.Projects.First();
+            return base.InitializeVariablesAsync(state);
+        }
+    }
+}
---
+++ examples/Kkts.Examples/Kkts.Examples.VariablesAndInOperator/Program.cs
@@ -0,0 +1,38 @@
+﻿using Kkts.Examples.VarablesAndInOperator;
+using Kkts.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+var context = new TestDbContext(TestDbContext.Options);
+var resolver = new CustomVariableResolver(context);
+
+// init variables: user.id and project.*
+await resolver.InitializeVariablesAsync(null!);
+
+var predicate = await Interpreter.ParsePredicateAsync<User>("id in [$user.id]", variableResolver: resolver);
+var predicate2 = await Interpreter.ParsePredicateAsync<User>("id in $userIds", variableResolver: resolver);
+var predicate3 = await Interpreter.ParsePredicateAsync<User>("id in [1, 2, 3]", variableResolver: resolver);
+
+var users = await context.Users.Where(predicate.Result).ToListAsync();
+var users2 = await context.Users.Where(predicate2.Result).ToListAsync();
+var users3 = await context.Users.Where(predicate3.Result).ToListAsync();
+
+Console.WriteLine("---------------------------------------");
+foreach (var user in users)
+{
+    Console.WriteLine(user);
+}
+
+Console.WriteLine("---------------------------------------");
+foreach (var user in users2)
+{
+    Console.WriteLine(user);
+}
+
+Console.WriteLine("---------------------------------------");
+foreach (var user in users3)
+{
+    Console.WriteLine(user);
+}
+
+
+Console.ReadKey();
